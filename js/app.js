464ade7e6a8370fe9a1e9d2ca79767cb5d4b9194
Carlos Roa <carlosroab95@gmail.com>
2023-04-29 15:26:30 -0300

    mejoras

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const carrito = document.querySelector("#carrito");
 const listaCursos = document.querySelector("#lista-cursos");
 const contenerdoCarrito = document.querySelector("#lista-carrito tbody");
 const vaciarCarrito = document.querySelector("#vaciar-carrito");
+let articulosCarrito = [];
 
 //console.log(carrito);
 //console.log(listaCursos);
@@ -16,6 +17,20 @@ function cargarEvent() {
 function agregarCurso(e) {
   e.preventDefault();
   if (e.target.classList.contains("agregar-carrito")) {
-    console.log(e.target);
+    const cursoEscogido = e.target.parentElement.parentElement;
+    leerDatos(cursoEscogido);
   }
+}
+
+function leerDatos(curso) {
+  console.log(curso);
+
+  const infoCurso = {
+    imagen: curso.querySelector("img").src,
+    titulo: curso.querySelector("h4").textContent,
+    precio: curso.querySelector(".precio span").textContent,
+    id: curso.querySelector("a").getAttribute("data-id"),
+  };
+
+  console.log(infoCurso);
 }
